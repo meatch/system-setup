dc0feb70a220d0e1b8a300e89ab6f7f032e6367e
Mitch Gohman <meatched@gmail.com>
2022-01-29 08:46:35 -0700

    Updating Oh My Zsh Installer to --unattended
    Oh My Zsh was kicking us out of my installer script, this flag ensure my script can continue so student should not have to do it twice.

--- MacOS-Setup.sh
+++ MacOS-Setup.sh
@@ -56,7 +56,7 @@ if [ -d ~/.oh-my-zsh ]; then
     echo "Oh-My-Zsh Already installed"
 else
     echo "Install Oh-My-Zsh"
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
 
 ###################
